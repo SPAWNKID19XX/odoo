c79066501e75176747f3d2091da1dc7b9a24cd26
spawnkid19xx <borisisac@gmail.com>
2023-12-04 22:48:41 +0000

    [ADD] Created really basic module for recording logic of odoo

--- custom/addons/car/__manifest__.py
+++ custom/addons/car/__manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "school_management",
-    'license': "LGLP-3",
+    'name': "car",
+    'license': "LGPL-3",
 
     'summary': """
-    teste module to remembering things about odoo
-    """,
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        will be created a module for marking a workdays
+        Long description of module's purpose
     """,
 
     'author': "Boris Isac",
-    'website': "https://www.yourcompany.com",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,9 +25,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/cars.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
