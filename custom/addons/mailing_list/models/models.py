b930d12bd54c560e54fe000dfb22ae795c3d4dbd
spawnkid19xx <borisisac@gmail.com>
2023-12-07 13:30:05 +0000

    [UPDATE]create a new user, fuel a avatar_picture.optimaze

--- custom/addons/mailing_list/models/models.py
+++ custom/addons/mailing_list/models/models.py
@@ -18,29 +18,45 @@ class MailingList(models.Model):
         string="Gender"
     )
     name = fields.Char(string="Name")
+    email = fields.Char(string="Email")
+    phone = fields.Char(string="Phone")
+    picture = fields.Image("Image")
+
     street = fields.Char(string="Street")
     city = fields.Char(string="City")
     state = fields.Char(string="State")
     country = fields.Char(string="Country")
     postcode = fields.Char(string="Postcode")
-    email = fields.Char(string="Email")
-    phone = fields.Char(string="Phone")
-    picture = fields.Image()
 
     API_LINK = 'https://randomuser.me/api'
 
     def create_user(self):
-        print('---hello world ONE')
         json_data = requests.get(self.API_LINK).json()
+        picture_url = json_data['results'][0]['picture']['large']
+        decoded_img = base64.b64encode(requests.get(picture_url).content)
+        context = {
+            'gender': json_data['results'][0]['gender'],
+            'name': json_data['results'][0]['name']['first'] + ' ' + json_data['results'][0]['name']['last'],
+            'email': json_data['results'][0]['email'],
+            'phone': json_data['results'][0]['phone'],
+            'picture': decoded_img,
+            'street': json_data['results'][0]['location']['street']['name'] + ' ' + str(
+                json_data['results'][0]['location']['street']['number']),
+            'city': json_data['results'][0]['location']['city'],
+            'state': json_data['results'][0]['location']['state'],
+            'country': json_data['results'][0]['location']['country'],
+            'postcode': json_data['results'][0]['location']['postcode']
+        }
 
-        print('+++', json_data)
-        print('---', json_data['results'][0])
+        return context
 
-        self.email = json_data['results'][0]['email']
-        self.gender = json_data['results'][0]['gender']
-        self.name = json_data['results'][0]['name']['first'] + ' ' + json_data['results'][0]['name']['last']
-        self.phone = json_data['results'][0]['phone']
-        self.picture = json_data['results'][0]['picture']['large']
+    def create_one_user(self):
+        print('---hello world ONE')
+        user = self.create_user()
+        if user['name']:
+            self.write(user)
+        else:
+            self.create(user)
 
     def create_few_user(self):
         print('---hello world FEW')
