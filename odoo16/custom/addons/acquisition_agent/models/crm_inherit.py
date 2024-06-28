d99625efa465a0dc523e5a8919bce99345c4ccc1
boris-isac <borisisac@gmail.com>
2024-06-28 02:08:53 +0100

    [ADD] function witch send from crm.lead to sale order a acquisition_agent by self.env.context['acquisition_agent']

--- odoo16/custom/addons/acquisition_agent/models/crm_inherit.py
+++ odoo16/custom/addons/acquisition_agent/models/crm_inherit.py
@@ -5,3 +5,9 @@ class CrmLead(models.Model):
     _description = 'crm inherit add acquisition_agent field'
 
     acquisition_agent = fields.Many2one('res.partner',  string="Acquisition agent")
+
+    def action_sale_quotations_new(self):
+        res = super(CrmLead, self).action_sale_quotations_new()
+        if isinstance(res, dict) and res.get('context'):
+            res['context']['default_acquisition_agent'] = self.acquisition_agent.id #pass directly id
+        return res
--- odoo16/custom/addons/acquisition_agent/models/sale_order_inherit.py
+++ odoo16/custom/addons/acquisition_agent/models/sale_order_inherit.py
@@ -5,3 +5,11 @@ class Sales(models.Model):
     _description = 'sale order inherit add acquisition_agent field'
 
     acquisition_agent = fields.Many2one('res.partner',  string="Acquisition agent")
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(Sales, self).default_get(fields_list)
+        if self.env.context.get('default_acquisition_agent'):
+            res['acquisition_agent'] = self.env.context['default_acquisition_agent']
+            self.acquisition_agent = res['acquisition_agent']
+        return res
